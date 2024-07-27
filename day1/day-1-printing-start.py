2aa5279471bf3b92cdd375d59b2fc78e2a2831c0
Magdalena Oreshkova <63170223+schrodingerisdead@users.noreply.github.com>
2024-07-27 13:23:49 +0200

    python day1

---
+++ day1/day-1-printing-start.py
@@ -0,0 +1,19 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like  this:")
+print("print('what to print')")
+# String Manipulation
+print("Hello world!\nHello Magi")
+# string concatenation
+print("Hello"+"Magi")
+print("Hello"+" "+"Magi")
+# input
+print(len(input("What is your name?")))
+# variables
+name=input("What is your name?")
+print(name)
+
+name="Jack"
+print(name)
+
+name="Angela"
+print(name)
